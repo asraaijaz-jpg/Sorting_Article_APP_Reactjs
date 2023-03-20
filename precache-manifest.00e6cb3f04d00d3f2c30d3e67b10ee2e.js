66367e2cc5cc857fdc82134f933ee3f6b8e3bb11
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-20 19:33:24 +0500

    Updates

---
+++ precache-manifest.00e6cb3f04d00d3f2c30d3e67b10ee2e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b95b14951f316219c67e9ce12d2d22",
+    "url": "/Sorting_Article_APP_Reactjs/index.html"
+  },
+  {
+    "revision": "0918442b49ae0008ee8a",
+    "url": "/Sorting_Article_APP_Reactjs/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "a944e1322f129ad7844c",
+    "url": "/Sorting_Article_APP_Reactjs/static/css/main.b81aef37.chunk.css"
+  },
+  {
+    "revision": "0918442b49ae0008ee8a",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/4.ab6d9d13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/4.ab6d9d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa89d0a8a7b55111208",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/5.7548f83d.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/5.7548f83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09b7826818dab0b909a",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/6.c40c6eca.chunk.js"
+  },
+  {
+    "revision": "a944e1322f129ad7844c",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/main.5964365c.chunk.js"
+  },
+  {
+    "revision": "0c8ea4c2db223d94e21a",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/runtime-main.6b1278e7.js"
+  },
+  {
+    "revision": "6cc46b11052f3d6fd3fb",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/stencil-h8k-logo_2-entry-js.90467e1e.chunk.js"
+  },
+  {
+    "revision": "fb49b299ad5029b7e010",
+    "url": "/Sorting_Article_APP_Reactjs/static/js/stencil-polyfills-css-shim.5391b34c.chunk.js"
+  }
+]);
